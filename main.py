9a5a29f783153136ed1e5c8acd4c406a376c3b24
François Sémécurbe <francoissemecurbe@gmail.com>
2026-04-16 12:33:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import duckdb
 from fastapi import FastAPI
+import logging
 
 def init_db():
     conn = duckdb.connect(database="production_conchylicole.duckdb", read_only=False)
@@ -19,6 +20,6 @@ def create_producer(siret: str = "123456789", commune: str = "29019", espece: st
     conn = duckdb.connect(database="production_conchylicole.duckdb", read_only=False)
     query = f"INSERT INTO conchyliculture (siret, commune, espece, tonnes) VALUES (?, ?, ?, ?)"
     conn.execute(query, (siret, commune, espece, tonnes))
-    print(conn.execute('select * from conchyliculture'))
+    logger.info(conn.execute('select * from conchyliculture'))
     conn.close()
     return {"message": f"Merci pour votre dépot de données siret : {siret}"}
